fbc0a9cfdf836395430c63df80da587b1724fb99
root <simon.ball.1985@gmail.com>
2017-03-08 12:11:17 +0800

    Update the environment setup (Docker)

--- initial.sh
+++ initial.sh
@@ -1,14 +1,62 @@
 #!/bin/bash
-echo "###################################################"
+#!/
+#!/# Author Simon Ball <contact@simonball.me>
+# This is just a quick shell script to bootstrap Docker Symfony projects
+
+# Function to check whether command exists or not
+exists()
+{
+  if command -v $1 &>/dev/null
+  then
+    return 0
+  else
+    return 1
+    fi
+}
+
+# Start by checking whether docker and docker-compose exist
+echo "Checking whether needed tools found"
+if exists docker
+  then echo "Docker found"
+  else echo "Docker not found, exiting"
+  exit
+fi
+
+if exists docker-compose
+  then echo "Docker-compose found"
+  else echo "Docker-compose not found, exiting"
+  exit
+fi
+
 echo "Creating an environment file to use in docker build"
-echo "###################################################"
-echo "WEB_BASE=/var/www/html" > .env
 echo "USER_ID=$UID" >> .env
 echo "GROUP_ID=$(id -g)" >> .env
-echo "###################################################"
+
 echo "Building Docker files. This may take some time"
-echo "###################################################"
 docker-compose build
-echo "###################################################"
-echo "Complete! Now type 'docker-compose up' to run"
-echo "###################################################"
+
+echo "Starting up Docker environment"
+docker-compose up -d
+
+# Wait until docker is available
+until docker exec -t firstworldtest_project_toolchain_1 ls -la > /dev/null 2>&1; do sleep 2; done
+
+echo "Docker environment is up and running"
+
+echo "Running Composer install"
+docker exec -it symfonytutorial_project_toolchain_1 composer install --no-interaction
+echo "Dumping Assets"
+docker exec -it symfonytutorial_project_toolchain_1 php bin/console assets:install --symlink --relative
+
+echo "### NOTES ###"
+echo "To get back to your parent system"
+echo "exit"
+echo "To shutdown the environment, once you are back in your parent shell"
+echo "docker-compose stop"
+echo "In the future, if you want to boot up the environment again"
+echo "docker-compose up"
+echo "In the future, to get a shell in the toolchain, once Docker is up"
+echo "docker exec -it firstworldtest_project_toolchain_1 bash"
+echo "### END NOTES ###"
+echo "Ready for you to run. Getting an interactive shell prompt in the project directory. Try running phing!"
+docker exec -it symfonytutorial_project_toolchain_1 bash
