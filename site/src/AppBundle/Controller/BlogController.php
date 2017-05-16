33cab01795643a4ec32e4c2aa463f234f36234b6
Simon <simon.ball.1985@gmail.com>
2017-05-16 09:37:12 +0800

    tutorial resources

--- site/src/AppBundle/Controller/BlogController.php
+++ site/src/AppBundle/Controller/BlogController.php
@@ -5,6 +5,11 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+// Include functionality for form submission
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+// Include the Data Entity
+use AppBundle\Entity\Comment;
+
 class BlogController extends Controller
 {
     /**
@@ -28,14 +33,47 @@ class BlogController extends Controller
     /**
      * @Route("/blog/{id}", name="blog_singular")
      */
-    public function singularAction($id)
-    {
-      $post = $this->getDoctrine()
-      ->getRepository('AppBundle:Post')
-      ->find($id);
+     public function singularAction($id, Request $request)
+     {
+       /* Form definition */
+       $post = $this->getDoctrine()
+       ->getRepository('AppBundle:Post')
+       ->find($id);
 
-      // replace this example code with whatever you need
-      return $this->render('blog/singular.html.twig', ['post' => $post]);
-    }
+       // Prepare a data object for use with the form
+       $comment = new Comment();
+       $form = $this->createFormBuilder($comment)
+         ->add('email')
+         ->add('body')
+         // Submit isn't part of data object so we add it
+         ->add('save', SubmitType::class, array('label' => 'Submit comment'))
+         ->getForm();
+
+       // Get Symfony to parse the form
+       $form->handleRequest($request);
+       if ($form->isSubmitted() && $form->isValid())
+       {
+
+         /* Set some comment properties */
+         $comment->setPost($post);
+         $comment->setCreatedAt(new \DateTime());
+
+         // Save the comment
+         $em = $this->getDoctrine()->getManager();
+         $em->persist($comment);
+         $em->flush();
+
+         $request->getSession()->getFlashBag()
+         ->add("alerts", array(
+           "type" => "success",
+           "message" => "Thanks for submitting some feedback!"));
+       }
+
+       return $this->render('blog/singular.html.twig', array(
+         'post' => $post,
+         'comment_form' => $form->createView(),
+       ));
+
+     }
 }
 
